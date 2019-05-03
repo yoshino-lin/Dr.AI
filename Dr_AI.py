2e149fa70125bef31a8cb992f2cddeca216d590c
Yudong Lin <37920527+yoshino-lin@users.noreply.github.com>
2019-05-04 05:14:03 +0800

    Add files via upload

---
+++ Dr_AI.py
@@ -0,0 +1,8 @@
+# -&- coding: utf-8 -*-
+import json,os
+import linecache
+
+employees_load = (len(open("employees\skyfire.yml",'r',encoding='utf-8').readlines())) #获取干员信息
+maps_load = (len(open("maps\CE-3.yml",'r',encoding='utf-8').readlines())) #地图信息
+
+
