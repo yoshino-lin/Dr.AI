d9365e4d965b382ed35455e7a44d004791d6e1a6
Yudong Lin <37920527+yoshino-lin@users.noreply.github.com>
2019-05-07 05:14:19 +0800

    Add files via upload

--- Dr_AI.py
+++ Dr_AI.py
@@ -8,9 +8,14 @@ employees_load = (len(open("employees\skyfire.yml",'r',encoding='utf-8').readlin
 maps_load = (len(open("maps\CE-3.yml",'r',encoding='utf-8').readlines())) #地图信息
 
 #地图载入
-map_data_list = []
-size_info = linecache.getline("maps\CE-3",2)
+map_file = "maps\CE-3"
+map_height_info = linecache.getline(map_file,3)#获取地图宽度，宽度等于加载的循环次数
+map_data = [] #创建地图数据的list
+#讲地图数据导入到list当中
+for i in range(map_height_info-1):
+	map_data.append(map_height_info = linecache.getline(map_file,5+i))
 
-def employee_load(employee,level,reliability,elite_lv,proficiency_lv) #干员名称，等级，信赖度，精英化等级，潜能突破等级
+#加载干员
+def employee_load(employee,level,reliability,elite_level,proficiency_level) #干员名称，等级，信赖度，精英化等级，潜能突破等级
 	employee_file_name= employee + ".yml"
 	hp_value = linecache.getline(employee_file_name,5)
