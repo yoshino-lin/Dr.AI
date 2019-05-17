7fb02f56ddbe48103f3fc108e2636d5ed7e12b8b
Yudong Lin <37920527+yoshino-lin@users.noreply.github.com>
2019-05-17 12:44:17 +0800

    Add files via upload
    我开始有点犯懒了...

--- Dr_AI.py
+++ Dr_AI.py
@@ -2,26 +2,25 @@
 import json,os
 import linecache
 import yaml
-import time
-#截图
-import win32gui, win32ui, win32con, win32api
-#图片识别
-import pytesseract
-from PIL import ImageGrab, Image
-
-#读取文件
-employees_load = (len(open("employees\skyfire.yml",'r',encoding='utf-8').readlines())) #获取干员信息
-maps_load = (len(open("maps\CE-3.yml",'r',encoding='utf-8').readlines())) #地图信息
 
 #地图载入
-map_file = "maps\CE-3"
+map_file = "maps\CE-3.yml"
+maps_load = (len(open(map_file,'r',encoding='utf-8').readlines())) #地图信息
 map_height_info = linecache.getline(map_file,3)#获取地图宽度，宽度等于加载的循环次数
 map_data = [] #创建地图数据的list
+
 #讲地图数据导入到list当中
-for i in range(map_height_info-1):
-	map_data.append(map_height_info = linecache.getline(map_file,5+i))
+for i in range(1,int(map_height_info)):
+	map_data.append(linecache.getline(map_file,4+i))
 
 #加载干员
-def employee_load(employee,level,reliability,elite_level,proficiency_level) #干员名称，等级，信赖度，精英化等级，潜能突破等级
-	employee_file_name= employee + ".yml"
-	hp_value = linecache.getline(employee_file_name,5)
+employees_data_list = []
+def employee_load(employee,level,reliability,elite_level,proficiency_level): #干员名称，等级，信赖度，精英化等级，潜能突破等级
+	global employees_data_list #获取干员数据存放列表的操作权限
+	employee_file_path = "employees\\"+ employee + ".yml" #干员文件的路径
+	#读取干员信息并以字典的形式存入列表当中
+	employees_data_list.append(yaml.load(open(employee_file_path, 'r', encoding='utf-8'), Loader=yaml.FullLoader))
+	print(employees_data_list)
+	
+	
+employee_load("skyfire",1,1,1,1)
